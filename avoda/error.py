6942a195ba71412c050d622d6fc1808a8f8ba80d
innas2019 <inna.i.shuster@gmail.com>
2024-04-01 21:06:32 +0300

    errors

---
+++ avoda/error.py
@@ -0,0 +1,16 @@
+from flask import render_template,Blueprint
+from werkzeug.exceptions import HTTPException
+bp = Blueprint("errs", __name__)
+
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', title="ничего не найдено"), 404
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+
+    return render_template('404.html',title="у нас проблемы"), 500
+
+@bp.app_errorhandler(Exception)
+def not_found_error(error):
+    return render_template('404.html',title="у вас проблемы"), Exception
