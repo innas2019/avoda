e2689da41d08a9e4aad592ec5b432f5537e3250a
innas2019 <inna.i.shuster@gmail.com>
2024-06-05 11:42:45 +0300

    menu and other cosmetics

--- avoda/error.py
+++ avoda/error.py
@@ -1,20 +1,23 @@
 from flask import render_template,Blueprint
 from werkzeug.exceptions import HTTPException
 bp = Blueprint("errs", __name__)
-
 @bp.app_errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html', title="страница не найдена"), 404
+    return render_template('404.html', title="Страница не найдена"), 404
 
 @bp.app_errorhandler(400)
 def internal_error(error):
 
-    return render_template('404.html',title="ошибка в приложении"), 400
+    return render_template('404.html',title="Ошибка в приложении"), 400
 
 @bp.app_errorhandler(500)
 def internal_error(error):
 
-    return render_template('404.html',title="у нас проблемы"), 500
+    return render_template('404.html',title="Ошибка сервер"), 500
+@bp.route("/err")
+def custom():
+    return render_template('404.html',title="Функция пока не реализована, зарегистрируйтесь еще раз с другим логином"), 500
+
 """ 
 @bp.app_errorhandler(Exception)
 def not_found_error(error):
